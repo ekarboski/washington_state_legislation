c6b40ecf09724ab93c6c1123efbb143aed05bffc
Emily Karboski <eskarboski@gmail.com>
2018-12-05 17:00:55 -0800

    Add percert yea loaders and reorganize page

--- zeta/wa_app.py
+++ zeta/wa_app.py
@@ -1,8 +1,35 @@
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
+from web_functions import select_one_bill_from_label_df, get_bill_text
 
 app = Flask(__name__, static_url_path="")
 
+@app.route('/')
+def wa():
+    """Return the main page."""
+    selected_senate_table, selected_house_table, rep_score, dem_score = select_one_bill_from_label_df('2299')
+    senate_table = selected_senate_table.to_dict(orient='records')
+    house_table = selected_house_table.to_dict(orient='records')
+    bill_text = get_bill_text('ESHB 2299')
+    return render_template('wa.html', senate_table=senate_table, house_table=house_table, 
+                            rep_score=rep_score, dem_score=dem_score, bill_text=bill_text)
+
+@app.route('/about')
+def about():
+    """Return the main page."""
+    return render_template('wa-about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Return the main page."""
+    return render_template('wa-contact.html')
+
+
+
+
+
+# OLD, FROM TEMPLATE
 @app.route('/main')
 def index():
     """Return the main page."""
@@ -22,18 +49,3 @@ def elements():
 def blog():
     """Return the main page."""
     return render_template('blog.html')
-
-@app.route('/contact')
-def contact():
-    """Return the main page."""
-    return render_template('wa-contact.html')
-
-@app.route('/')
-def wa():
-    """Return the main page."""
-    return render_template('wa.html')
-
-@app.route('/about')
-def about():
-    """Return the main page."""
-    return render_template('wa-about.html')
--- zeta/web_functions.py
+++ zeta/web_functions.py
@@ -12,6 +12,8 @@ def load_label_data():
 
 
 def select_one_bill_from_label_df(bill_num):
+    '''Select bill from label_df that matches the inputted bill_num and return pandas dataframes of 
+    the senate and the house, as well as the rep_score and dem_score for the bill.'''
     label_df = load_label_data()
     selected_label_df = label_df[label_df['bill_num'] == bill_num]
 
@@ -23,4 +25,15 @@ def select_one_bill_from_label_df(bill_num):
     sorted_house = house_selected_label_df.sort_values('last_name')
     sorted_house = sorted_house.reset_index().drop('index', axis=1)
 
-    return sorted_senate, sorted_house
+    rep_score = sorted_senate.loc[0, 'rep_score']
+    dem_score = 1 - rep_score
+
+    return sorted_senate, sorted_house, rep_score, dem_score
+
+def get_bill_text(bill_id):
+    engine = create_engine('postgresql://localhost:5432/wa_leg_staging')
+    current_bill_text_df = pd.read_sql_query('select * from "current_bill_text"',con=engine)
+    bill = current_bill_text_df[current_bill_text_df['bill_id'] == 'ESHB 2299']
+    return bill.iloc[0, -1]
+
+
