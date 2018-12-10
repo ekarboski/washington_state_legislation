38f078eca32b0d1c3af3f678dd6653e477126e0c
Emily Karboski <eskarboski@gmail.com>
2018-12-10 13:01:05 -0800

    Change hard coded table to loop

--- zeta/wa_app.py
+++ zeta/wa_app.py
@@ -3,24 +3,28 @@ from flask import Flask, request, render_template, jsonify
 from web_functions import select_one_bill_from_label_df, get_bill_text, calculate_lean_percentages
 
 app = Flask(__name__, static_url_path="")
+# Interesting bills to showcase: 2145 - split votes, 2265 - split sponsors
 
 @app.route('/')
 def wa():
     """Return the main page."""
-    # Interesting bills to showcase: 2145 - split votes, 2265 - split sponsors
-    bill_id = 'HB 2145'
+    kwargs = get_bill_info()
+    return render_template('wa.html', **kwargs)
+
+def get_bill_info(bill_id='HB 2145'):
+    """Selects bill"""
     selected_senate_table, selected_house_table, rep_score, dem_score, bill_url = select_one_bill_from_label_df(bill_id)
     senate_table = selected_senate_table.to_dict(orient='records')
     house_table = selected_house_table.to_dict(orient='records')
     bill_text = get_bill_text(bill_id)
     s_leaning_yea, s_leaning_nay, s_undecided = calculate_lean_percentages(selected_senate_table)
     h_leaning_yea, h_leaning_nay, h_undecided = calculate_lean_percentages(selected_house_table)
-    linkedin = "https://www.linkedin.com/in/emilykarboski"
-    return render_template('wa.html', senate_table=senate_table, house_table=house_table, 
+    return dict( senate_table=senate_table, house_table=house_table, 
                             rep_score=rep_score, dem_score=dem_score, bill_text=bill_text,
                             s_leaning_nay=s_leaning_nay, s_leaning_yea=s_leaning_yea, s_undecided=s_undecided, 
                             h_leaning_nay=h_leaning_nay, h_leaning_yea=h_leaning_yea, h_undecided=h_undecided, 
-                            bill_url=bill_url, bill_id=bill_id, linkedin=linkedin)
+                            bill_url=bill_url, bill_id=bill_id)
+
 
 @app.route('/about')
 def about():
